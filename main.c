cdd29935547de5bd497ccf9e2f3d3646bc2f7b51
Saeed Mahmoodi <saeed.m201@gmail.com>
2023-06-13 11:59:30 +0330

    Structuring client connection.

--- main.c
+++ main.c
@@ -1,30 +1,42 @@
+// Copyright 2023 Saeed Mahmoodi
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
-#define PORT 8001
+#define BUFFER_SIZE 4096
+#define PORT 8002
+#define BACKLOG 200
 #define IP "127.0.0.1"
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
+
 int
 main() {
+    struct connection {
+        int fd;
+        int buffsize;
+        int addrlen;
+        char buff[BUFFER_SIZE];
+    };
+
+    int sfd;
     struct sockaddr_in server_addr;
     struct sockaddr_in client_addr;
-    int sfd;
-    int clientfd;
-    int clientlen;
-    char readbuff[1024];
- 
-	
+    struct connection client_conn;
+    client_conn.buffsize = BUFFER_SIZE;
+
     // Create socket
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd == -1) {
-        handle_error("Socket creation failed.")
+        handle_error("Socket creation failed.");
     }
 
     // Setup address to bind
@@ -40,21 +52,23 @@ main() {
     }
 
     // Listening for new connection
-    if (listen(sfd, 10) != 0) {
+    if (listen(sfd, BACKLOG) != 0) {
         handle_error("Listening for new connection failed.");
     }
 
     // Accepting connection
-    clientlen = sizeof(client_addr);
-    clientfd = accept(sfd, (struct sockaddr *) &client_addr, &clientlen);
-    if (clientfd == -1) {
+    client_conn.addrlen = sizeof(client_addr);
+    client_conn.fd = accept(sfd, (struct sockaddr *) &client_addr,
+            &client_conn.addrlen);
+
+    if (client_conn.fd == -1) {
         handle_error("Accepting connection failed.");
     }
 
-    while(1) {
-        bzero(readbuff, 1024);
-        read(clientfd, readbuff, 1024);
-        write(clientfd, readbuff, 1024);
+    while (1) {
+        bzero(client_conn.buff, client_conn.buffsize);
+        read(client_conn.fd, client_conn.buff, client_conn.buffsize);
+        write(client_conn.fd, client_conn.buff, client_conn.buffsize);
     }
 
     close(sfd);
